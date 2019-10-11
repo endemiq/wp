675c1143356f213d57c7fc02b1e096704bd628aa
Yann Gouffon <github@yago-art.ch>
2019-10-11 09:47:36 +0200

    ♻️ change php files organisation

--- wp-content/themes/endemiq/functions.php
+++ wp-content/themes/endemiq/functions.php
@@ -1,13 +1,3 @@
 <?php
 
-// Enable ACF config import/export
-add_filter('acf/settings/save_json', function ($path) {
-  return get_stylesheet_directory() . '/acf';    
-});
-
-add_filter('acf/settings/load_json', function ($paths) {
-  unset($paths[0]);
-  $paths[] = get_stylesheet_directory() . '/acf';
-  return $paths;
-});
- 
+require_once('config/acf.php');
