85b50c9e28aa456bb6e1382d91ab91ba8af3cae8
Yann Gouffon <github@yago-art.ch>
2019-10-11 09:41:03 +0200

    ✨ enable ACF json import

--- wp-content/themes/endemiq/functions.php
+++ wp-content/themes/endemiq/functions.php
@@ -1,7 +1,13 @@
 <?php
 
-// Enable ACF config export
+// Enable ACF config import/export
 add_filter('acf/settings/save_json', function ($path) {
   return get_stylesheet_directory() . '/acf';    
+});
+
+add_filter('acf/settings/load_json', function ($paths) {
+  unset($paths[0]);
+  $paths[] = get_stylesheet_directory() . '/acf';
+  return $paths;
 });
  
